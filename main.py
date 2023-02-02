8d58c0f42eda214662a7ee7bfc0a4643f5616924
Dhruv Desai <dhruvdesai222@gmail.com>
2023-02-02 10:35:16 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+
+# import fuction/class from library
+# libarary is predevloped programs
+
+# from (library) import (class)
+
+from fractions import Fraction
+
+
+# variable example
+my_string = "Hello World"
+print (my_string)
+
+# Types of Variable
+
+#string
+print (type(my_string))
+#int
+print(type(1))
+#float
+print(type(1.0))
+#boolean (Case sensative True/False)
+print (type(True))
+#list
+print (type([1,2]))
+#dictionary
+print (type({"1":1,"2":2}))
+
+
+# addition is only happen with similar variables
+
+a=2
+b=Fraction(1.5)
+c= "Dhruv"
+d= "Desai"
+
+print(a+b)
+print(c+d)
